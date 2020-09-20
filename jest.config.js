b7882b99605c06e8974cf25c612f7c5cf698155f
markostanimirovic <markostanimirovic95@gmail.com>
2020-09-20 21:26:17 +0200

    jest setup; added tests for string-helper

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  roots: ['<rootDir>/projects/ngrx-handlers'],
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
---
+++ projects/ngrx-handlers/src/lib/handlers.spec.ts
@@ -0,0 +1,23 @@
+describe('handlers', () => {
+  describe('combineHandlers', () => {
+    it('should create actions', () => {
+      expect(true).toBe(true);
+    });
+
+    it('should create a reducer', () => {
+      expect(true).toBe(true);
+    });
+  });
+
+  describe('plain', () => {
+    it('should return a plain reducer', () => {
+      expect(true).toBe(true);
+    });
+  });
+
+  describe('withPayload', () => {
+    it('should return a plain reducer', () => {
+      expect(true).toBe(true);
+    });
+  });
+});
